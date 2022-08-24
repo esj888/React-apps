b01909432a7095bd5e5d8d2592f4a48197e41552
eric <eric.arakaki@gmail.com>
2022-08-23 21:09:09 -1000

    initial commit

---
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import ExpenseDate from './ExpenseDate';
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  //   const expenseDate = new Date(2022, 7, 21);
+  //   const expenseTitle = "iPhone 14";
+  //   const expenseAmount = 1099.0;
+
+  return (
+    <div className="expense-item">
+        <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
