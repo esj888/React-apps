1d0909357b9e912fe010cabffd710decf5be07c5
eric <eric.arakaki@gmail.com>
2022-11-29 19:02:48 -1000

    completed sec 5-65

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);  // must be called in component functions.
+  //const [title, setTitle] = useState(props.title);  // must be called in component functions.
 
-    const clickHandler = () => {
-      setTitle('Updated!');   // pass new value to be updated. component function will execute again.
-      console.log({title});   // variable title doesn't get updated right away.
-  };
+  // const clickHandler = () => {
+  //     setTitle('Updated!');   // pass new value to be updated. component function will execute again.
+  //     console.log(title);   // variable title doesn't get updated right away.
+  // };
 
   return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
